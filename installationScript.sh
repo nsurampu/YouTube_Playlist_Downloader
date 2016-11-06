646e172a3304d477e3eece0a71e7f83b429bd8f3
Naren Surampudi <nsurampu@gmail.com>
2016-11-06 23:46:22 +0530

    Update installationScript.sh

--- installationScript.sh
+++ installationScript.sh
@@ -2,7 +2,7 @@
 # this will install all the packages necessary for running this project
 
 pip install --upgrade pip #installs pip
-sudo pip install --upgrade youtube-dl #installs youtube-dl
+pip install --upgrade youtube-dl #installs youtube-dl
 pip install pytube #installs pytube
 pip install lxml #installs lxml
 echo "Installation successful. You may now run the project."
